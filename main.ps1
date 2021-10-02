2a62d26195c68c567f1da7fafdcdcf177d318a16
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2021-10-02 23:22:06 +0800

    feature-ps1: Create new `.xlsx` for calculations

--- main.ps1
+++ main.ps1
@@ -30,11 +30,26 @@ foreach ($file in $files) {
 
 $current_path = Get-Location
 
-# Get name of folder
+# Get name of folder containing data
 Set-Location $path_to_file_directory
 
 $current_folder = Split-Path -Path (Get-Location )-Leaf
 
+
+# Creating a new .xlsx file
+$name_of_new_excel_xlsx = -join ($current_folder, '_data_calculations.xlsx')
+
+$full_path_to_new_excel = -join ($path_to_file_directory, -join ('\', $name_of_new_excel_xlsx ))
+
+$new_excel = New-Object -ComObject excel.application
+$new_excel.visible = $False
+
+$new_excel_workbook = $new_excel.Workbooks.Add()
+$new_excel_workbook.SaveAs($full_path_to_new_excel) 
+$new_excel.Quit()
+
+
+# Set location back to the current script path
 Set-Location $current_path
 
 # Pipe array consisting of file names and directory path to Python
