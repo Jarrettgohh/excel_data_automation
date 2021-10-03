5f88d50956274f0232bae248d412a9e98974ea3b
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2021-10-03 10:56:59 +0800

    # Reading pandas dataframe at certain row and col

--- pandas_excel.py
+++ pandas_excel.py
@@ -1,4 +1,25 @@
 import pandas as pd
 
-data = pd.read_excel('./R100_D5.xlsx', skiprows=27, usecols=[2])
-print(data)
+
+def excel_read_col_row(excel_file, ):
+    df = pd.read_excel(excel_file, skiprows=25, usecols=[2])
+
+
+# df = data frame
+# skiprows (+1)
+# usecols row A = pandas col 0
+df = pd.read_excel('pandas.xlsx', skiprows=25, usecols=[2])
+# COL
+# -> Row A in Excel is considered as row index 0 in pandas
+
+
+# ROW
+# -> df row given from pandas is (-2) index for the row number
+# -> The second row index (row 2 in Excel) starts from 0 in pandas
+# -> Due to pandas counting the row 1 in Excel to be the header
+
+
+# range = for each index (-skiprows value given)
+loc = df.loc[range(0, 10)]
+print(loc)
+# print(data_frame.loc[27])
