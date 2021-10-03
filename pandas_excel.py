793b342a32d7ee6c35228a3f29948ff38e595b44
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2021-10-03 11:53:20 +0800

    # Added function to read index in Excel file from data passed in from config.json file

--- pandas_excel.py
+++ pandas_excel.py
@@ -1,14 +1,36 @@
 import pandas as pd
+import json
 
 
-def excel_read_col_row(excel_file, ):
-    df = pd.read_excel(excel_file, skiprows=25, usecols=[2])
+def excel_read_col_row(excel_file, row_col_dict):
 
+    rows = row_col_dict['rows']
+    rows_range = range(rows[0]-2, rows[-1]-1)
+
+    usecols = row_col_dict['cols']
+
+    df = pd.read_excel(excel_file,  usecols=usecols)
+
+    df = df.loc[rows_range]
+
+    print(df)
+
+
+json_file = open('config.json', 'r')
+row_col_dict = json.load(json_file)
+row_col_dict = row_col_dict['index_to_read']
+
+
+excel_read_col_row('pandas.xlsx', row_col_dict=row_col_dict)
 
 # df = data frame
-# skiprows (+1)
+# skiprows (-2)
 # usecols row A = pandas col 0
-df = pd.read_excel('pandas.xlsx', skiprows=25, usecols=[2])
+# df = pd.read_excel('pandas.xlsx',
+#                    # skiprows=25,
+#                    usecols=[2])
+
+
 # COL
 # -> Row A in Excel is considered as row index 0 in pandas
 
@@ -20,6 +42,6 @@ df = pd.read_excel('pandas.xlsx', skiprows=25, usecols=[2])
 
 
 # range = for each index (-skiprows value given)
-loc = df.loc[range(0, 10)]
-print(loc)
+# loc = df.loc[range(0, 10)]
+# print(df)
 # print(data_frame.loc[27])
