b504f35f0e84c384720d2927ff18b5e5b6ebbabc
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2021-10-04 12:40:52 +0800

    # Test: Format according to device sizes

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import sys
 import json
 import pandas as pd
-import numpy as np
+# import numpy as np
+import re
 
 lines = sys.stdin.readlines()
+
+
+config_json = open('config.json', 'r')
+config_json = json.load(config_json)
+
 
 file_names = []
 directory = ''
@@ -38,55 +44,73 @@ def excel_read_col_row(excel_file, row_col_dict):
 
 
 capacitance_values_dict = {}
+device_dimensions = config_json['device_dimensions']
 
-# Loop through each file in folder
+# To sort the different wafer dimensions through the file name
 for file_name in file_names:
-    json_file = open('config.json', 'r')
-    row_col_dict = json.load(json_file)
-    row_col_dict = row_col_dict['index_to_read']
 
-    df = excel_read_col_row(
-        f'{directory}\{file_name}.xlsx', row_col_dict=row_col_dict)
-
-    capacitance_values_dict[file_name] = df.to_numpy()
-
-
-for wafer_name in capacitance_values_dict:
-    capacitance_data_nested = capacitance_values_dict[wafer_name]
-
-    capacitance_data = []
-
-    for data in capacitance_data_nested:
-        capacitance_data.append(data[0])
-
-    capacitance_values_dict[wafer_name] = capacitance_data
+    for index, device_dimension in enumerate(device_dimensions):
+        print(device_dimension)
+        # device_dimension_regex = f'\{device_dimension}'
+        # print(device_dimension_regex)
+        # device_num = re.sub(device_dimension_regex, '', file_name, 1)
+        # print(device_num)
 
 
-# NOTES BELOW IS FOR 1 NEST ARRAY
+# # Loop through each file in folder
+# for file_name in file_names:
+#     row_col_dict = config_json
+#     row_col_dict = row_col_dict['index_to_read']
 
-# index could be found from number of element in each array
-# columns can be found from the number of array in the array (nested array)
+#     df = excel_read_col_row(
+#         f'{directory}\{file_name}.xlsx', row_col_dict=row_col_dict)
 
-# Example [['a', 'b', 'c'], ['d', 'e', 'f']],
-# -> `index` array arg should have 3 elements
-# -> `columns` array arg should have 2 elements
-
-
-# np_array = np.array([capacitance_values_dict['S100_D2'],
-#                      capacitance_values_dict['S100_D3']])
-
-df = pd.DataFrame(
-    data=capacitance_values_dict,
-    index=[list(range(1, 11))],
-    columns=['S100_D1', 'S100_D2',
-             'S100_D3', 'S100_D4', 'S100_D5']
-)
-
-print(df)
+#     capacitance_values_dict[file_name] = df.to_numpy()
 
 
-try:
-    df.to_excel(f'{directory}\\Test_1_data_calculations.xlsx')
+# capacitance_values_dict (EXAMPLE):
+# {
+#  'S100_D1':
+# [[0.451], [0.545], [0.5435], [0.6969]],
+#  'S100_D2':
+# [[0.411], [0.525], [0.5455], [0.689]],
+# }
 
-except:
-    print('\n\nFailed to write to new excel file. Make sure the file is not open.')
+
+# # Loop through each wafer size to
+# for wafer_size in capacitance_values_dict:
+#     capacitance_data_nested = capacitance_values_dict[wafer_size]
+
+#     capacitance_data = []
+
+#     # Replace nested array with normal array (capacitance_values_dict); for each wafer size
+#     for data in capacitance_data_nested:
+#         capacitance_data.append(data[0])
+
+#     capacitance_values_dict[wafer_size] = capacitance_data
+
+
+# device_numbers = []
+
+# for device in capacitance_values_dict:
+#     device_regex = '\S100_'
+
+#     device_num = re.sub(device_regex, '', device, 1)
+
+#     device_numbers.append(device_num)
+
+
+# df = pd.DataFrame(
+#     data=capacitance_values_dict,
+#     # index=[list(range(1, 11))],
+#     # columns=device_numbers
+# )
+
+# print(df)
+
+
+# try:
+#     df.to_excel(f'{directory}\\Test_1_data_calculations.xlsx')
+
+# except:
+#     print('\n\nFailed to write to new excel file. Make sure the file is not open.')
