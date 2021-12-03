fc3b34ef198851e6138b83a49698eab64f63a32d
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2021-12-03 16:30:51 +0800

    # Updated `endurance_text.py`: Write to `.xlsx` file instead using `openpyxl`

--- endurance_text.py
+++ endurance_text.py
@@ -1,20 +1,25 @@
-
+import openpyxl
 from Excel.excel_functions import append_df_to_excel
 import pandas
 import xlwt
 import re
+import json
 
-txt_file = './Endurance/endurance_test_data.txt'
-excel_file = './Endurance/endurance_test_data.xls'
-excel_transfer_file = './Endurance/data_transfer.xls'
+config_json = open('config.json', 'r')
+config_json = json.load(config_json)
 
-book = xlwt.Workbook()
-ws = book.add_sheet('data')  # Add a sheet
+txt_file = './Endurance/endurance_test_data_PV.txt'
+excel_file = './Endurance/endurance_test_data.xlsx'
+
+wb = openpyxl.Workbook()
+ws = wb.worksheets[0]
+
+# book = xlwt.Workbook()
+# ws = book.add_sheet()  # Add a sheet
 
 file = open(txt_file, 'r+')
 
 data = file.readlines()  # read all lines at once
-
 
 for row_index in range(len(data)):
     # This will return a line of string data
@@ -22,18 +27,22 @@ for row_index in range(len(data)):
 
     for data_index in range(len(row)):
         row_data = re.sub('Â', '', row[data_index])
-        ws.write(row_index, data_index, row_data)  # Write to cell i, j
+        ws.cell(row=row_index + 1, column=data_index + 1).value = row_data
 
+        # ws.write(row_index, data_index, row_data)  # Write to cell i, j
 
-book.save(excel_file)
+wb.save(excel_file)
 file.close()
 
-
 # Read with pandas
-df = pandas.read_excel(excel_file, sheet_name='data',
-                       usecols='C:D')
+# df = pandas.read_excel(excel_file, sheet_name='data', usecols='C:D')
 
-voltage_polarization_data = df.iloc[40:542]
+# voltage_polarization_data = df.iloc[40:542]
 
-append_df_to_excel(df=voltage_polarization_data, filename=excel_transfer_file,
-                   sheet_name='data_transfer', startrow=2, startcol=2,)
+# append_df_to_excel(
+#     df=voltage_polarization_data,
+#     filename=excel_file,
+#     sheet_name='data_transfer',
+#     startrow=2,
+#     startcol=2,
+# )
