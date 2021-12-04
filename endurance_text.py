96201bccd48cfe7d6595bb1c7de706eef81eb087
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2021-12-04 08:24:50 +0800

    # Updated `config.json` and variable name

--- endurance_text.py
+++ endurance_text.py
@@ -11,6 +11,8 @@ config_json = json.load(config_json)
 txt_file = './Endurance/endurance_test_data_PV.txt'
 excel_file = './Endurance/endurance_test_data.xlsx'
 
+sheet_name = 'data_transfer'
+
 wb = openpyxl.Workbook()
 ws = wb.worksheets[0]
 
@@ -25,9 +27,9 @@ for row_index in range(len(data)):
     # This will return a line of string data
     row = data[row_index].split()
 
-    for data_index in range(len(row)):
-        row_data = re.sub('Â', '', row[data_index])
-        ws.cell(row=row_index + 1, column=data_index + 1).value = row_data
+    for col_index in range(len(row)):
+        row_data = re.sub('Â', '', row[col_index])
+        ws.cell(row=row_index + 1, column=col_index + 1).value = row_data
 
         # ws.write(row_index, data_index, row_data)  # Write to cell i, j
 
@@ -35,14 +37,14 @@ wb.save(excel_file)
 file.close()
 
 # Read with pandas
-# df = pandas.read_excel(excel_file, sheet_name='data', usecols='C:D')
+df = pandas.read_excel(excel_file, sheet_name=sheet_name, usecols='C:D')
 
-# voltage_polarization_data = df.iloc[40:542]
+voltage_polarization_data = df.iloc[40:542]
 
-# append_df_to_excel(
-#     df=voltage_polarization_data,
-#     filename=excel_file,
-#     sheet_name='data_transfer',
-#     startrow=2,
-#     startcol=2,
-# )
+append_df_to_excel(
+    df=voltage_polarization_data,
+    filename=excel_file,
+    sheet_name=sheet_name,
+    startrow=2,
+    startcol=2,
+)
