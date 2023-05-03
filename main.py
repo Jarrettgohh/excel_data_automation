b505c570419edc9d364ba91a0dbed1741e989245
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2023-05-03 15:26:31 +0800

    #

--- main.py
+++ main.py
@@ -5,34 +5,28 @@ import re
 from option_2 import option_2
 from functions import execute_powershell, execute_powershell_function
 
-print('-----------------------------')
-print('\nRemeber to edit the config.json file\n')
-print('-----------------------------')
-print('-----------------------------')
-print('\nRun options\n')
-print('-----------------------------')
-print('\n')
-print(
-    '1. Transfer data from text (.txt) file to excel (.xlsx) file. Select this option if you wish to update the config.json file according to a new excel file cell format.'
-)
-print(
-    '2. Extract data from .txt, .csv or .xls file into a .xlsx file in a certain format.'
-)
+option_2()
 
-print('-----------------------------')
-user_selection = input('Enter your choice: ')
-print('\n')
+# print('-----------------------------')
+# print('\nRemember to edit the config.json file\n')
+# print('-----------------------------')
+# print('-----------------------------')
+# print('\nRun options\n')
+# print('-----------------------------')
+# print('\n')
+# print(
+#     '1. Transfer data from text (.txt) file to excel (.xlsx) file. Select this option if you wish to update the config.json file according to a new excel file cell format.'
+# )
+# print(
+#     '2. Extract data from .txt, .csv or .xls file into a .xlsx file in a certain format.'
+# )
 
+# print('-----------------------------')
+# user_selection = input('Enter your choice: ')
+# print('\n')
 
-    
+# if user_selection == "1":
+#     sys.exit()
 
-if user_selection == "1":
-    sys.exit()
-
-elif user_selection == "2":
-    option_2()
-
-
-    
-    
-    
+# elif user_selection == "2":
+#     option_2()
